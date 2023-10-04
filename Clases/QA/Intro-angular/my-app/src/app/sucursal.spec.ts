ac6be95e0b9cd20d4473b03e4381e2d1b5aed277
Jose <joseserranoq00@gmail.com>
2023-10-03 19:04:32 -0600

    talle clase 10 hecho

--- Clases/QA/Intro-angular/my-app/src/app/sucursal.spec.ts
+++ Clases/QA/Intro-angular/my-app/src/app/sucursal.spec.ts
@@ -54,10 +54,43 @@ describe('Sucursal', () => {
     expect(function () {
       cliente.retirar(withdrawlAmount2000, numeroCuenta);
     }).toThrowError(Error, "Fondos insuficientes");
-
     verify(mockedCuenta.retirar(withdrawlAmount2000)).never();
   });
 
+  it('a. Se abre una cuenta y se verifica que el monto minimo es 5000', function () {
+    let cuenta3 = instance(mockedCuenta);
+    cuenta3.setCantidadDinero(5000);
+    cliente.setCuentas(cuenta3);
+    when(mockedCuenta.getCantidadDinero()).thenReturn(5000);
+    expect(cliente.getCuentas()[1].getCantidadDinero()).toBe(5000);
+  });
 
+  it('b. La cuenta esta en 0, se realizan 2 depositos validos y se verifique que el saldo de la cuenta sea igual al monto de los 2 depositos', function(){
+    let cuenta4 = instance(mockedCuenta);
+    cuenta4.setCantidadDinero(0);
+    cliente.setCuentas(cuenta4);
+    let withdrawAmount3000 = 300000;
+    cliente.getCuentas()[1].depositar(withdrawlAmount2000);
+    cliente.getCuentas()[1].depositar(withdrawAmount3000);
+    when(mockedCuenta.getCantidadDinero()).thenReturn(withdrawAmount3000 + withdrawlAmount2000);
+    expect(cliente.getCuentas()[1].getCantidadDinero()).toBe(500000);
+  });
 
+  it('c. Al liquidar la cuenta de un cliente en una suscursal, el metodo devuelve el saldo correcto de la cuenta',function(){
+    let cuenta5 = instance(mockedCuenta);
+    let saldo = 5000;
+    cuenta5.setCantidadDinero(saldo);
+    cliente.setCuentas(cuenta5);
+
+    when(mockedCuenta.getCantidadDinero()).thenReturn(saldo);
+    when(mockedCuenta.liquidar()).thenReturn(saldo);
+    expect(cliente.getCuentas()[1].liquidar()).toBe(5000);
+  });
+  it('d. Verifique que al liquidar una cuenta una cuenta de  una cliente de una sucursal, que la cantidad de cuentas disminuye en 1',function(){
+    let cuenta6 = instance(mockedCuenta);
+    cliente.setCuentas(cuenta6);
+    when(mockedCuenta.liquidar()).thenCall(()=>{cliente.getCuentas().pop()});
+    cliente.getCuentas()[1].liquidar();
+    expect(cliente.getCuentas().length).toBe(1);
+  });
 });
